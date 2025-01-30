0d91e7f462c33a385acd1a7189c02bc22955a2d2
indianup <jaibajarangclasses@gmail.com>
2025-01-30 23:22:59 +0530

    Delete utils/wvd_check.py

--- utils/wvd_check.py
+++
@@ -1,8 +0,0 @@
-import os
-import glob
-
-def wvd_check():
-    wvd_files = glob.glob(f'{os.getcwd()}/WVDs/*.wvd')
-    if not wvd_files:
-        raise FileNotFoundError("No .wvd file found in the WVDs directory")
-    return wvd_files[0]
